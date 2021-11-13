ae323cfa68dd006788467e9c408a8279c2377dba
Sarah Ghanei <sarahghanei1997@gmail.com>
2021-11-13 15:59:19 +0330

    zarinpal gate configs added

--- SRC/OnlineShopping/orders/urls.py
+++ SRC/OnlineShopping/orders/urls.py
@@ -5,4 +5,6 @@ app_name = 'orders'
 urlpatterns = [
     path('create/', create_order, name='create'),
     path('<int:order_id>/', order_detail, name='detail'),
+    path('payment/<int:order_id>/<price>', payment, name='payment'),
+    path('verify/', verify, name='verify'),
 ]
--- SRC/OnlineShopping/orders/views.py
+++ SRC/OnlineShopping/orders/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import *
 from shop.shop import Shop
-
+from suds.client import Client
+from django.http import HttpResponse
+from django.contrib import messages
 
 # Create your views here.
 @login_required
@@ -21,3 +23,47 @@ def create_order(request):
 def order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     return render(request, 'orders/order_detail.html', {'order': order})
+
+
+MERCHANT = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+client = Client('https://www.zarinpal.com/pg/services/WebGate/wsdl')
+description = 'صفحه پرداخت Emart'
+# email and mobile of customer included in the receipt
+# both are optional
+mobile = '09121231200'
+CallbackURL = 'http://localhost:8000/orders/verify/'
+
+
+@login_required
+def payment(request, order_id, price):
+    # first version
+    # make amount global so it can be accessible in verify view
+    global amount, o_id
+    amount = price
+    o_id = order_id
+    result = client.service.PaymentRequest(MERCHANT, amount, description, request.user.email, mobile, CallbackURL)
+    if result.Status == 100:
+        return redirect('https://zarinpal.com/pg/StartPay/' + str(result.Authority))
+    else:
+        return HttpResponse('Error code: ' + str(result.Status))
+
+
+@login_required
+# after payment gateway we come here to verify the payment
+def verify(request):
+    # first version
+    if request.GET.get('Status') == 'OK':
+        result = client.service.PaymentVerification(MERCHANT, request.GET['Authority'], amount)
+        if result.Status == 100:
+            # set the paid variable True
+            order = Order.objects.get(id=o_id)
+            order.paid = True
+            order.save()
+            messages.success('Transaction was successful', 'success')
+            return redirect('cart:home')
+        elif result.Status == 101:
+            return HttpResponse('Transaction Submitted Before')
+        else:
+            return HttpResponse('Error code')
+    else:
+        return HttpResponse('Transaction failed or canceled by user')
