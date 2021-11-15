154643feb3d4ffe0d78dfb93382ea3c0d5a6b99a
Sarah Ghanei <sarahghanei1997@gmail.com>
2021-11-15 19:33:57 +0330

    log configs, and log file have been set

--- SRC/OnlineShopping/OnlineShopping/settings.py
+++ SRC/OnlineShopping/OnlineShopping/settings.py
@@ -137,3 +137,41 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Logging configurations
+
+LOGGING = {
+    'version': 1,
+    # The version number of our log
+    'disable_existing_loggers': False,
+    # django uses some of its own loggers for internal operations. In case you want to disable them just replace the
+    # False above with true.
+    # formatters for handlers
+    # formatters will be applied on handlers not loggers
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s %(asctime)s %(module)s: %(message)s'
+        }
+    },
+    # A handler for WARNING. It is basically writing the WARNING messages into a file called
+    # WARNING.log
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'log_info.log',
+            'formatter': 'simple',
+        },
+
+    },
+    # A logger for WARNING which has a handler called 'file'. A logger can have multiple handler
+    'loggers': {
+        # notice the blank '', Usually you would put built in loggers like django or root here based on your needs
+        '': {
+            'handlers': ['file'],  # notice how file variable is called in handler which has been defined above
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+
+}
--- SRC/OnlineShopping/orders/signals.py
+++ SRC/OnlineShopping/orders/signals.py
@@ -2,12 +2,19 @@ from django.db.models.signals import post_save
 from product.models import Product
 from .models import Order, OrderItem
 from django.dispatch import receiver
+import logging
+
+# __name__ is the name of a python module that contains the logger
+logger = logging.getLogger(__name__)
 
 
+# update inventory of items in the order
 @receiver(signal=post_save, sender=OrderItem)
 def update_inventory(sender, instance, **kwargs):
     instance.product.inventory -= instance.quantity
     print("Inventory updated!")
+    logger.info('Product: {} Inventory:{} '.format(instance.product.name, instance.product.inventory))
     if instance.product.inventory == 0:
         instance.product.available = False
+        logger.warning('Product {} unavailable.'.format(instance.product.name))
     instance.product.save()
