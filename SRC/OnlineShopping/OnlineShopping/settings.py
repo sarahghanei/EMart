07c16cfe5d3574df52321a2a7249b219a865a8c8
Sarah Ghanei <sarahghanei1997@gmail.com>
2021-11-15 20:28:53 +0330

    hiding secret key

--- SRC/OnlineShopping/OnlineShopping/settings.py
+++ SRC/OnlineShopping/OnlineShopping/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nc15i*g@=_ny8&m)9yg6lws3pbvmgtg0dh4jz*^495x)q14$f4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = config("SECRET_KEY")  # this is to replace the secret key you cut away before
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
